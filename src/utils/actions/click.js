21eb950fb5947514b40a95820ee2dc534c018be8
gleb.lobastov <gleb.lobastov@gmail.com>
2020-11-01 23:57:37 +0300

    feat: allow to click on item found by querySelectorAll by index

--- src/utils/actions/click.js
+++ src/utils/actions/click.js
@@ -1,16 +1,37 @@
 export default async function click(
   page,
   selector,
-  { visible, hidden, waitTimeout = 5000, button, clickCount, clickDelay } = {}
+  {
+    visible,
+    hidden,
+    waitTimeout = 5000,
+    button,
+    clickCount,
+    clickDelay,
+    selectorIndex = 0
+  } = {}
 ) {
   await page.waitForSelector(selector, {
     visible,
     hidden,
     timeout: waitTimeout
   });
-  await page.click(selector, {
+
+  const clickOptions = {
     button,
     clickCount,
     delay: clickDelay
-  });
+  };
+
+  if (selectorIndex > 0) {
+    const elementHandles = await page.$$(selector);
+    if (!elementHandles[selectorIndex]) {
+      throw new Error(
+        `Can't find element by selector ${selector} at index ${selectorIndex}. Total: ${elementHandles.length} elements`
+      );
+    }
+    await elementHandles[selectorIndex].click(clickOptions);
+  } else {
+    await page.click(selector, clickOptions);
+  }
 }
