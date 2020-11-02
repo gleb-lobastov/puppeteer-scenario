7472b962bfdef0216a431374f77081a8d23dbd79
gleb.lobastov <gleb.lobastov@gmail.com>
2020-11-02 20:34:51 +0300

    fix: don't catch errors in objMapAsync
    Otherwise such errors is silenced

--- src/utils/objMapAsync.js
+++ src/utils/objMapAsync.js
@@ -1,9 +1,7 @@
 export default function objMapAsync(object, callback) {
   return Promise.all(
     Object.entries(object).map(([key, value]) => {
-      return Promise.resolve(callback(value))
-        .catch(error => error)
-        .then(result => [key, result]);
+      return Promise.resolve(callback(value)).then(result => [key, result]);
     })
   ).then(results =>
     results.reduce((accumulator, [key, result]) => {
